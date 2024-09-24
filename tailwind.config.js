5b1f9671f28be5734e30637e1e0512202c975639
Milos <srejic.milos94@gmail.com>
2024-09-24 13:10:09 +0200

    Modified tailwindcss configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,29 @@
 module.exports = {
   content: ["./index.html", "./app.js"],
   theme: {
+    // Centering container and giving it some padding by default
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+      },
+    },
     extend: {
-      screens: {
-        phone: "640px",
-        tablet: "768px",
-        laptop: "1024px",
-        desktop: "1280px",
+      // Figtree font
+      fontFamily: {
+        figtree: ["Figtree", "sans-serif"],
+      },
+      // Adding custom colors
+      colors: {
+        textDark: "#0F172A",
+        textLight: "#F7F9FC",
+        textHighlight: "#0EA5E9",
+        bgDark: "#334155",
+        bgLight: "#E9EDFC",
+        lightPurple: "#6366F1",
+        lightBlue: "#DEF1FF",
+        darkBlue: "#06B6D4",
+        lightRose: "#FCDAEB",
       },
     },
   },
